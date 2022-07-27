e858ddf2dc1b334672841481032d7d2bdc093e5c
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-27 03:27:37 -0300

    Update server.js

--- todo-list-express-main/server.js
+++ todo-list-express-main/server.js
@@ -1,4 +1,4 @@
- // import express module
+ // Import express module - Allows the use of Express
 const express = require('express')
 // assign app function to express simplyfing it
 const app = express()
@@ -6,13 +6,13 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 // set the port of our application to 2121
 const PORT = 2121
-// set our environment variable file and requiring it
+// set our environment variable file and requiring it. This helps keeping the password of the database private.
 require('dotenv').config()
 
 // declare our database url and names
 let db,
     // declaring our database password with an environment variable
-    //This will not be uploaded to github because it is in the .gitignore file
+    //This .env file will not be uploaded to github because it is in the .gitignore file
     dbConnectionStr = process.env.DB_STRING,
     // declaring our database name 
     dbName = 'todo'
@@ -21,16 +21,16 @@ let db,
 // connect to our database using our environment variable
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     
-        .then(client => {
+        .then(client => { 
             //console to let user know connection to database was successful
         console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+        db = client.db(dbName) 
     })
 
 // Middlewares and other routes here... 
 // set our view engine to ejs
 app.set('view engine', 'ejs')
-// set our express server to use public folder "public"
+// set our express server to use public folder "public" for client-side files
 app.use(express.static('public'))
 // set our express server default to use the url encoded format
 app.use(express.urlencoded({ extended: true }))
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-// Read (GET)- Get something
+// Read (GET)- Get something and await for a response
 app.get('/',async (request, response)=>{
     // Get all the todos items from the database
     const todoItems = await db.collection('todos').find().toArray()
@@ -59,20 +59,20 @@ app.get('/',async (request, response)=>{
 })
 
 
-//Ask server to add a Todo item to Mongodb in the body with completed status set as false
+//Create (Post) - Ask server to add a Todo item to Mongodb in the body with completed status set as false
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
         // console log when item has been added
         console.log('Todo Added')
-       //refresh page to see new todo item
+       //refresh page to see new todo item - This is a new read request, not an update.
         response.redirect('/')
     })
     // catch error if something goes wrong
     .catch(error => console.error(error))
 })
 
-//This section is where a task would be marked as complete, once completed by the user and updated in MongoDB
+//Update (Put) - This section is where a task would be marked as complete, once completed by the user and updated in MongoDB
 app.put('/markComplete', (request, response) => {
     //updating items status completed or not  
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -82,8 +82,8 @@ app.put('/markComplete', (request, response) => {
           }
     },{
         //setting status to false 
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, //Sort by _id, making the element appear at the bottom.
+        upsert: false  //Update and insert false. Don't want to have the same document twice in the db
     })
     .then(result => {
         // console log if item has be completed
@@ -96,7 +96,7 @@ app.put('/markComplete', (request, response) => {
 
 })
 
-//Updates Mongodb to change status of Item to Uncomplete
+//Update (Put) - Updates Mongodb to change status of Item to Uncomplete
 app.put('/markUnComplete', (request, response) => {
     //updating items status completed or not  
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -105,9 +105,9 @@ app.put('/markUnComplete', (request, response) => {
             completed: false
           }
     },{ 
-        //setting status to false
-        sort: {_id: -1},
-        upsert: false
+        //setting status to false 
+        sort: {_id: -1}, //Sort by _id, making the element appear at the bottom. I think this was badly copypasted and should be 1 to put it at the top. 
+        upsert: false  //Update and insert false. Don't want to have the same document twice in the db
     })
     .then(result => {
         // console log if item has been completed
@@ -118,9 +118,9 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
-//It selects an element and tells the db to drop it from the collection. 
+//Deleté (Delete) - It selects an element and tells the db to drop it from the collection. 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) 
     .then(result => {
         
         console.log('Todo Deleted')  //If everything went ok, it logs the message in the console
